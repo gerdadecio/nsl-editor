e0700b92d03e72a20d3d5fe2c76eac0d7e69a0b1
iamgeeeee <contact@gerdadecio.com>
2026-09-21 15:28:48 +1000

    Add API audit display for reference details tab and  corresponding tests

--- test/controllers/references/tabs/for_editor/show_details_tab_hides_bulk_changed_when_api_at_missing_test.rb
+++ test/controllers/references/tabs/for_editor/show_details_tab_hides_bulk_changed_when_api_at_missing_test.rb
@@ -20,17 +20,30 @@ require "test_helper"
 
 # Single reference controller test.
 #
-# app/views/references/tabs/_tab_show_1.html.erb adds a "Record changed by"
-# audit line when @reference.api_name and @reference.api_at are both set.
-class ReferenceShowEditorDetailsTabShowsRecordChangedByWhenApiAuditPresentTest < ActionController::TestCase
+# app/views/references/tabs/_tab_show_1.html.erb renders
+# AuditHelper#updated_by_api_and_when, which adds nothing when
+# @reference.api_at is blank - even if api_name is set.
+class ReferenceShowEditorDetailsTabHidesBulkChangedWhenApiAtMissingTest < ActionController::TestCase
   tests ReferencesController
   setup do
     @reference = references(:simple)
-    @api_at = Time.zone.local(2026, 9, 21, 14, 5)
-    @reference.update_columns(api_name: "sample-api-user", api_at: @api_at)
   end
 
-  test "shows a Record changed by line when api_name and api_at are set" do
+  test "hides the Bulk changed line when api_name and api_at are blank" do
+    @reference.update_columns(api_name: nil, api_at: nil)
+    show_details_tab
+    assert_no_match(/Bulk changed/, response.body)
+  end
+
+  test "hides the Bulk changed line when only api_name is set" do
+    @reference.update_columns(api_name: "sample-api-user", api_at: nil)
+    show_details_tab
+    assert_no_match(/Bulk changed/, response.body)
+  end
+
+  private
+
+  def show_details_tab
     @request.headers["Accept"] = "application/javascript"
     get(:show,
         params: { id: @reference.id, tab: "tab_show_1" },
@@ -38,7 +51,5 @@ class ReferenceShowEditorDetailsTabShowsRecordChangedByWhenApiAuditPresentTest <
                    user_full_name: "Fred Jones",
                    groups: ["edit"] })
     assert_response :success
-    expected = "Record changed by: sample-api-user #{I18n.l(@api_at, format: :default)}"
-    assert_match(/#{Regexp.escape(expected)}/, response.body)
   end
 end
